2988945ecf2e45823e82a2692eda942ceda69932
gabrielc42 <clark.gabe97@gmail.com>
2025-01-07 19:38:28 -0600

    links, .powi correction, some unfinished business

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ fn main() {
     let c_c = num::complex::Complex::new(3, 2);
     let c_d = num::complex::Complex::new(2, 1);
     
-    // let c_a_exponent = c_a.scale(3);
-    let c_a_exponent = c_a.powi(3); // exponent2, .powi returns correct solution, .scale in Trait needs work
-    // println!("CA num = {}, .scale CA Trait exponent = {}, .powi i32 exponent = {}\n", c_a, c_a_exponent, c_a_exponent2);
+    let c_a_exponent = c_a.scale(3);
+    let c_a_exponent2 = c_a.powi(3); // exponent2, .powi returns correct solution, .scale in Trait needs work
+    println!("CA num = {}, .scale CA Trait exponent = {}, .powi i32 exponent = {}\n", c_a, c_a_exponent, c_a_exponent2);
 
-    let c_b_exponent = c_b.scale(3 as i32);
+    let c_b_exponent = c_b.powi(3);
     let c_c_exponent = c_c.powi(3);
     let c_d_exponent = c_d.powi(2);
 
@@ -28,3 +28,6 @@ fn main() {
 }
 
 // https://autumnai.github.io/cuticula/num/complex/struct.Complex.html
+
+// https://www.wolframalpha.com/input?i=%28%281%2B2i%29%5E3+-+%281-i%29%5E3%29+%2F+%28%283%2B2i%29%5E3+-+%282%2Bi%29%5E2%29
+// notice the result and the polar coordinates, consider a simple coding equation practice with those patterns
